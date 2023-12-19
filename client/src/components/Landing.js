d27f8337a673a390823a9ae7e99269cd464b0bb2
virtualblakewillis <bewillis101@gmail.com>
2023-12-19 14:52:05 -0600

    current page looks closer to final project, have visuals for cannot take quiz until log in/sign up, but no functionality working just yet

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,6 +2,7 @@ import hero from '../images/hero_logo.png';
 import './Landing.css';
 import { LogInOverlay, SignUpOverlay } from './LandingOverlays.js'
 import QuizQuestionComponent from './quizQuestion.js';
+import './notLoggedIn.css'
 
 function Landing() {
   function turnOnOverlay() {
@@ -16,9 +17,12 @@ function Landing() {
         <img src={hero} className="hero-logo" alt="are you: Lost in Space?" />
         <button className="primary cta">Take Our Quiz!</button>
         <QuizQuestionComponent />
-        <button className="primary" onClick={turnOnOverlay}>Log in</button>
-        <p>or</p>
-        <button className="secondary" onClick={turnOnSignUp}>Sign up</button>
+        <div className="notLoggedIn"></div>
+        <div className="notLoggedInButtons">
+          <button className="primary" onClick={turnOnOverlay}>Log in</button>
+          <p>or</p>
+          <button className="secondary" onClick={turnOnSignUp}>Sign up</button>
+        </div>
       </header>
       <LogInOverlay />
       <SignUpOverlay />
